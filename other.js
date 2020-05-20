e3478d58b9770798c0269e5cd945323419b03ee7
Vladimir <vladimirleonov789@gmail.com>
2020-05-20 14:22:40 +0300

    lesson 20 defined the length of the array using object.keys and cycle for

--- other.js
+++ other.js
@@ -1,3 +1,33 @@
-let test = "test";
 
-console.log(test.toUpperCase(test));
+"use strict";
+
+const options = {
+   name: 'test',
+   width: 1024,
+   height: 1024,
+   colors: {
+      border: 'black',
+      bg: 'red'
+   }
+}
+
+for (let key in options) {
+   if (typeof (options[key]) === 'object') {
+      for (let i in options[key]) {
+         console.log(`Свойство ${i} имеет значение: ${options[key][i]}`);
+      }
+   } else {
+      console.log(`Свойство ${key} имеет значение: ${options[key]}`);
+   }
+}
+
+let arrayObj = Object.keys(options);
+let numberOfElements;
+
+for (let i = 1; i <= arrayObj.length; i++) {
+   numberOfElements = i;
+}
+
+console.log(numberOfElements);
+console.log(arrayObj);
+console.log(options);
