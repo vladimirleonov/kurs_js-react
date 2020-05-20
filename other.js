f1d0d84a78c0b8e9e81b8ee546d2c319e058cd5d
Vladimir <vladimirleonov789@gmail.com>
2020-05-20 17:19:49 +0300

    make lesson 20 forach() for...of for...in

--- other.js
+++ other.js
@@ -1,27 +1,18 @@
 
 "use strict";
 
-const options = {
-   name: 'test',
-   width: 1024,
-   height: 1024,
-   colors: {
-      border: 'black',
-      bg: 'red'
-   }
+const arr = [2, 4, 6, 8, 10];
+
+// arr[19] = 30;
+// console.log(arr.length);
+// console.log(arr);
+
+arr.forEach(function (item, i, arr) {
+   console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+
+for (let key of arr) {
+   let i = 0;
+   console.log(`${i}: ${key} внутри массива ${arr}`);
+   i++;
 }
-
-for (let key in options) {
-   if (typeof (options[key]) === 'object') {
-      for (let i in options[key]) {
-         console.log(`Свойство ${i} имеет значение: ${options[key][i]}`);
-      }
-   } else {
-      console.log(`Свойство ${key} имеет значение: ${options[key]}`);
-   }
-}
-
-
-
-console.log((Object.keys(options)).length);
-console.log(options);
