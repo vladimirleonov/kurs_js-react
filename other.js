4a55a01c05ea5bfb068c9d4571f36f782c22689b
Vladimir <vladimirleonov789@gmail.com>
2020-05-21 19:26:59 +0300

    finished work with array lesson21

--- other.js
+++ other.js
@@ -1,18 +1,13 @@
 
 "use strict";
 
-const arr = [2, 4, 6, 8, 10];
-
-// arr[19] = 30;
-// console.log(arr.length);
+// const arr = [2, 4, 6, 8, 10];
 // console.log(arr);
 
-arr.forEach(function (item, i, arr) {
-   console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+// arr.forEach(function (item, i, arr) {
+//    console.log(`${i}: ${item} массива ${arr}`);
+// })
 
-for (let key of arr) {
-   let i = 0;
-   console.log(`${i}: ${key} внутри массива ${arr}`);
-   i++;
-}
+const name = [9, 13, 4, 12];
+name.sort();
+console.log(name);
