4a94a011c9850a27f36e1696da09a9cd4d8750fb
vladimir <vladimirleonov789@gmail.com>
2020-05-24 18:44:42 +0300

    finished the lesson 23 OOP

--- other.js
+++ other.js
@@ -1,54 +1,17 @@
 "use strict";
 
-// const obj = {
-//    a: 1,
-//    b: 2,
-//    c: {
-//       x: 3,
-//       y: 4
-//    }
-// }
 
-// function copy(mainObj) {
-//    const objCopy = {};
-//    for (let key in mainObj) {
-//       objCopy[key] = mainObj[key];
-//    }
-//    return objCopy;
-// }
+const soldier = {
+   health: 400,
+   armor: 100,
+   sayHello: function () {
+      console.log('Hello');
+   }
+};
 
-// const newObj = copy(obj);
-// newObj.a = 20;
-// console.log(obj);
-// console.log(newObj);
+const jonh = Object.create(soldier);
 
+Object.setPrototypeOf(jonh, soldier);
 
-
-
-
-const oldObj = {
-   a: 2,
-   b: 4
-}
-
-const newObj = { ...oldObj };
-newObj.a = 20;
-console.log(oldObj);
-console.log(newObj);
-
-const oldArray = [2, 4, 6];
-const newArray = [...oldArray];
-newArray[0] = 33;
-console.log(oldArray);
-console.log(newArray);
-
-
-
-
-function consoleLog(a, b, c) {
-   console.log(a);
-   console.log(b);
-   console.log(c);
-}
-const getUserAnsvers = [5, 7, 9];
-consoleLog(...getUserAnsvers);
+console.log(jonh.health);
+jonh.sayHello();
